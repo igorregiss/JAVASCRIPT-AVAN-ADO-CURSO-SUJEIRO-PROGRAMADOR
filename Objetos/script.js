6659ca833f03a11db491c79c3eca3d9249daf17c
Igor Régis <103765157+igorregiss@users.noreply.github.com>
2023-03-26 14:42:48 -0300

    Add files via upload

---
+++ Objetos/script.js
@@ -0,0 +1,26 @@
+/*
+let pessoa = {
+    nome:   "Igor",
+    idade: 21,
+    altura: 1.75,
+    cargo: "Desenvolvedor WEB" 
+};
+
+let carro = {
+    modelo: "SW4 SRX",
+    cor: "branca",
+    potencia: "200cv"
+};
+
+alert(carro.modelo);
+
+*/
+
+
+let usuarios = [
+    {nome: "Igor", cargo: "Desenvolvedor WEB", status: "ATIVO"},
+    {nome: "Regis", cargo: "Desenvolvedor Backend", status: "ATIVO"},
+    {nome: "Maria", cargo: "RH", status: "ATIVO"},
+]
+
+console.log(usuarios)
---
+++ Variaveis dentro de uma string/script.js
@@ -0,0 +1,10 @@
+// Template strings
+let nome = "Igor";
+let sobrenome = "Regis";
+let idade = 21;
+
+// let mensagem = "Meu nome é "+ nome +" "+ sobrenome +" e eu tenho "+ idade +" anos de idade.";
+
+let mensagem = `Meu nome é ${nome} ${sobrenome} e eu tenho ${idade} anos de idade`;
+
+console.log(mensagem);
